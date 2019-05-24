dbf3618d87242c8121f577259a030020bac472f6
Dominik Glodzik <dg17@sanger.ac.uk>
2019-05-24 13:52:36 +0100

    support for extra bed files and breakpoint counting

--- R/run_bg_model.R
+++ R/run_bg_model.R
@@ -18,23 +18,36 @@ normaliseVector <- function(v, refv=NULL) {
 
 # define a data frame with relevant features
 binSize = 5e5
-prepareData <- FALSE
+prepareData <- TRUE
 if (prepareData) {
     print('Loading the data ...')
-	allBins <- prepareBinData(maxBins=1000)
+    bedList <- c('/nfs/users/nfs_d/dg17/breast_rearr/data/metadata/dnase/MCF7DukeDNaseSeq.pk')
+    names(bedList) <- c('dnase')
+
+    # load the breakpoints
+    DATA.VERSION <- '29.11'
+    REARRANGEMENTS.PATH=paste0('/nfs/cancer_archive04/dg17/BASIS/versions/',DATA.VERSION,'/rearrangements')
+    PRELOAD.REARR.PATH<-paste(REARRANGEMENTS.PATH, '/allRearrangements.RData',sep='')
+    load(PRELOAD.REARR.PATH)
+    bps.sv1 <-  subset(r$sample.bps.all, Signature.SV1>0.5)
+    bpList=list()
+    bpList[[1]] <- bps.sv1
+    names(bpList)[1] <- 'counts.sv1'
+
+	allBins <- prepareBinData(maxBins=100, bedList=bedList, bpList=bpList)
 } else {
     load(paste0('../data/regressionData08.06-',binSize,'.RData'))
 }
 
                                         # regression part
                                         # prepare bins for regression
-bins.regression <- subset(allBins, noNbases<0.1*binSize &
-                              noCensusGenes==0 & !is.nan(meanCn))
+#bins.regression <- subset(allBins, noNbases<0.1*binSize &
+#                              noCensusGenes==0 & !is.nan(meanCn))
 bins.regression <- allBins
 
 ### run the regression model
 # choose the variables
-variables.included <- c(2,5,6)
+variables.included <- c(2,4,5,6)
 model.variables <- c('noNbases', 'medianRepTime', 'meanCn', 'dnase', 'highExpGenes', 'lowExpGenes', 'staining', 'fragile', 'counts.alu', 'counts.rep',
                      'segDup')[variables.included ]
 model.variable.names <- c('N bases', 'early replication', 'copy number', 'DNAse', 'highly expressed genes', 'lowly expressed genes', 'chromatin staining', 
--- R/utils/prepareBinData.R
+++ R/utils/prepareBinData.R
@@ -1,4 +1,7 @@
-prepareBinData <- function(maxBins=Inf) {
+prepareBinData <- function(maxBins=Inf,
+	bedList=c(), # vector of paths to bed files, with names of variables
+	bpList=NULL
+	) {
 	
 	# design the bins: 500kb bins every 250mb
     binList <- list()
@@ -16,12 +19,19 @@ prepareBinData <- function(maxBins=Inf) {
     gr.allBins <- GRanges(seqnames=Rle(paste0('chr',allBins$chr)),
                           ranges=IRanges(allBins$chromStart, allBins$chromEnd),
                           strand=rep(c("*"), nrow(allBins)))
+    	# down-sample the bins
+	if (maxBins!=Inf) {
+		si <- sample(nrow(allBins))
+		allBins <- allBins[si,]
+		gr.allBins <- gr.allBins[si,]
+	}
 
     # load the list of genes first
 	load('../../../data/biomart.19.05.2016.RData') # incl. genes.table
 
     # gene expression: highExpGenes', 'lowExpGenes'
-    
+
+
 	if (!exists('expr.data')) {
 	    expr.matrix <- read.table('/nfs/cancer_archive04/dg17/BASIS/expression/FPKM/QuantNorm_log2_FPKM_n342.txt', header=TRUE, sep='\t')
 	    expr.matrix.raw <- expr.matrix[, 6:ncol(expr.matrix )]
@@ -64,13 +74,43 @@ prepareBinData <- function(maxBins=Inf) {
                      )
 	overlaps.rep.domains <- as.data.frame(findOverlaps(gr.allBins, gr.timing ))
 
-	# down-sample the bins
-	if (maxBins!=Inf) {
-		allBins <- allBins[sample(nrow(allBins), maxBins),]
+
+	covList <- list()
+	# all the other bed files
+	for (bi in 1:length(bedList)) {
+		aBed <- read.table(bedList[bi])
+		colnames(aBed) <- c('chr', 'chromStart', 'chromEnd')
+		gr.bed <- GRanges(seqnames=Rle(paste0(aBed[,1])),
+                    ranges=IRanges(aBed[,2], aBed[,3]),
+                    strand=rep(c("*"), nrow(aBed)),
+                    seqlengths=seqlengths(Hsapiens)
+                    )
+        coverage.bed <-coverage(gr.bed)
+        covList[[bi]] <- coverage.bed
+
+        allBins[,names(bedList)[bi]] <- NA
+
+	}
+
+
+
+
+
+	# breakpoints
+	if (!is.null(bpList)) {
+		# for all breakpoint lists
+		for (bps.i in 1:length(bpList)) {
+			allBins[,names(bpList)[bps.i]] <- NA
+			gr.bps <- GRanges(seqnames=Rle(paste0('chr',bpList[[bps.i ]]$chr)),
+                    ranges=IRanges(bpList[[bps.i ]]$position, bpList[[bps.i ]]$position),
+                        strand=rep(c("*"), nrow(bpList[[bps.i ]])))
+			allBins[,names(bpList)[bps.i]] <-  countOverlaps(gr.allBins,gr.bps)
+		}
 	}
 
 	# Ssummarize all bins
     for (bi in 1:min(maxBins,nrow(allBins))) {
+    	print(bi)
 		allBins$highExpGenes[bi] <- sum(coverage.expr.genes [[paste0('chr', allBins$chr[bi])]][allBins$chromStart[bi]: allBins$chromEnd[bi]]>0)
         allBins$lowExpGenes[bi] <- sum(coverage.low.expr.genes[[paste0('chr', allBins$chr[bi])]][allBins$chromStart[bi]: allBins$chromEnd[bi]]>0)
        
@@ -78,6 +118,11 @@ prepareBinData <- function(maxBins=Inf) {
         if (nrow(matching.domains)>0) {
             allBins$medianRepTime[bi] <-  median(mcols(gr.timing[matching.domains$subjectHits,])$timing)
         }
+
+        # loop over the other bed files
+       	for (bl in 1:length(bedList)) {
+       		allBins[bi,names(bedList)[bl]] <-  sum(covList[[bl]][[paste0('chr', allBins$chr[bi])]][allBins$chromStart[bi]: allBins$chromEnd[bi]]>0)
+       	}
 
     }
     return(allBins)
