7c84ec8de178ad7bb89eb1fee2af990e15128752
Dominik Glodzik <dg17@sanger.ac.uk>
2019-06-06 05:05:50 +0100

    added example data for breast

--- R/run_preprocess_data.R
+++ R/run_preprocess_data.R
@@ -1,5 +1,5 @@
     # process the gene expression data
-genes.table <- read.csv('../data/genes.table.csv')
+genes.table <- read.csv('../data/genes.table.csv', row.names=NULL)[,2:7]
 expr.matrix <- read.table('/nfs/cancer_archive04/dg17/BASIS/expression/FPKM/QuantNorm_log2_FPKM_n342.txt', header=TRUE, sep='\t')
 expr.matrix.raw <- expr.matrix[, 6:ncol(expr.matrix )]
 expr.matrix.raw.t <- t(expr.matrix[, 6:ncol(expr.matrix )])
@@ -11,8 +11,10 @@ genes.high.expr <- genes.table
 genes.high.expr <- subset(genes.high.expr,ensembl_gene_id %in% subset(medianGeneExpression, exprQuantile=="gene.expr.Q4")$EnsemblID)
 genes.low.expr <- subset(genes.table,ensembl_gene_id %in% subset(medianGeneExpression, exprQuantile!="gene.expr.Q4")$EnsemblID)
         
-write.csv(genes.high.expr, file='../data/genes.high.expr.csv')
-write.csv(genes.low.expr , file='../data/genes.low.expr.csv')
+genes.high.expr$chr <- paste0('chr', genes.high.expr$chr)        
+write.table(genes.high.expr, file='../data/breastData/genes.high.expr.tsv')
+genes.low.expr$chr <- paste0('chr', genes.low.expr$chr)    
+write.table(genes.low.expr , file='../data/breastData/genes.low.expr.tsv')
 
 
    # chromosome staining
@@ -21,13 +23,13 @@ recreateBands <- FALSE
 if (recreateBands) {
     FILE.bands <- '../data/chromBands'
     bands <- read.table(FILE.bands, header=TRUE)
-    bands$chrom <- bands$chrom,4,100000L
+    bands$chrom <- substr(bands$chrom,4,100000L)
     bands$chromStart <- bands$chromStart +1
     bands <- bands [order(bands$chrom, bands$chromStart ),] # order the bands
     names(bands) <- c('chr', 'chromStart',  'chromEnd', 'name', 'gieStain')
     bands$isStaining <- bands$gieStain %in% c('gpos100', 'gpos25', 'gpos50', 'gpos75')
     bands <- subset(bands, isStaining==TRUE)
-    write.table(bands, file= '/nfs/users/nfs_d/dg17/breast_rearr/data/chromBands/chromBandsPos', sep='\t', quote=FALSE)
+    write.table(bands, file= '../data/breastData/chromBandsPos', sep='\t', quote=FALSE)
 }
 
         # load the breakpoints
@@ -38,4 +40,4 @@ load(PRELOAD.REARR.PATH)
 # prepare the breakpoint data
 # data frame with columns 'chr' (values without chr prefix) and 'posiiton'
 bps.sv1 <-  subset(r$sample.bps.all, Signature.SV1>0.5)
-write.csv(bps.sv1, file='../data/bps.sv1.csv')
+write.csv(bps.sv1, file='../data/breastData/bps.sv1.csv')
